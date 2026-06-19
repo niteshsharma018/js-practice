d43e4706ea9c8b876a4d7d84c9517ab2d179f9c6
niteshsharma018 <ns182210@gmail.com>
2026-06-20 00:00:54 +0530

    Add files via upload

---
+++ promise,all.js
@@ -0,0 +1,36 @@
+
+function task1() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("task1 -> Done")
+        }, 2000);
+    })
+}
+function task2() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("task2 -> Done")
+        }, 3000);
+    })
+}
+function task3() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("task3 -> Done")
+        }, 1000);
+    })
+}
+
+Promise.all([
+    task1(),
+    task2(),
+    task3()
+])
+.then((result) => {
+    console.log(result);
+    console.log("All tasks Completed");
+})
+.catch((err)=>{
+    console.log(err);
+    
+})
