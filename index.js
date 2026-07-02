cebb8b4a59e20f57df016dd56d60dc988a378be5
Nitesh kumar sharma <ns182210@gmail.com>
2026-07-02 20:24:58 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,74 +1,41 @@
-// class ComplexNumber {
-//     constructor(real, imaginary){
-//         this.real = real;
-//         this.imaginary = imaginary;
+console.log("This is a tutorial on loops")
 
-//     }
+let a = 1;
 
-//     add(secondCompplexNumber)
-// {
-//     let real = this.real + secondCompplexNumber.real;
-//     let imaginary = this.imaginary + secondCompplexNumber.imaginary;
-
-//     return new ComplexNumber(real, imaginary);
-// }
-
-// }
-
-// // let c = new ComplexNumber(3,4);
-// // console.log(c);
-
-// let c1 = new ComplexNumber(2,4);
-// let c2 = new ComplexNumber(5,6);
-
-// let result = c1.add(c2);
-// console.log(`${result.real} + ${result.imaginary}i`);
-
-
-// // let result = c1.add(c2);
-
-
-
-// // c.real();
-// // c.imaginary();
-
-
-// ttask 3
-
-class Human{
-    constructor(name, age){
-        this.name = name;
-        this.age = age;
-    }
-    introduce(){
-        console.log(`Hey,  I am ${this.name}`);
-    }
+for (let i = 0; i < 103; i++) {
+    console.log(a + i);
 }
 
-class Student extends Human{
-    constructor(naem, age, course){
-       super(name , age)
-       this.course = course;
+let obj = {
+    name: "Nitesh",
+    Role: "programer",
+    company: "nws"
 
-    }
-    introduce(){
-        super.introduce();
-        console.log(`I study ${this.course}`);
-    }
+}
+
+// for in loop
+for (const key in obj) {
+    
+    const element = obj[key];
+    console.log(key, element) 
+}
+
+for (const c of "Nitesh") {
+    console.log(c)
+    
+}
+
+// while loop
+
+let i = 0;
+while(i<6){
+    console.log(i)
+    i++;
 }
 
 
-let nage = new Human("Nitesh",19);
-console.log(nage);
-
-let stu = new Student("Nitesh",19,"B.tech(CSE)");
-console.log(stu);
-stu.introduce();
-
-console.log(Human instanceof Student);
-console.log(Human instanceof Human);
-
-console.log(stu instanceof Student);
-console.log(stu instanceof Human);
-
-
+let i = 10;
+do {
+    console.log(i);
+    i++
+} while (i<8);
